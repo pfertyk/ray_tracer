bc13ec2793073c821c9c4e62ce9708eb95451375
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-11-04 22:54:48 +0100

    Improved Lights.

--- raytracer/Lights.py
+++ raytracer/Lights.py
@@ -3,32 +3,32 @@ import numpy as np
 
 class Lamp:
     def __init__(self, color=(255, 255, 255), position=(0, 0, 0), max_lighting_distance=30):
-        self.color = color
-        self.position = position
+        self.color = np.array(color)
+        self.position = np.array(position)
         self.max_lighting_distance = max_lighting_distance
 
     def illuminates(self, point, scene):
-        light_vector = np.subtract(self.position, point)
+        light_vector = self.position - point
         light_vector_len = np.linalg.norm(light_vector)
         return not scene.check_collision(point, light_vector / light_vector_len, far=light_vector_len)
 
     def get_light_intensity_at(self, point):
-        distance = np.linalg.norm(np.subtract(point, self.position))
+        distance = np.linalg.norm(point - self.position)
         if distance > self.max_lighting_distance:
-            return 0, 0, 0
+            return np.array((0, 0, 0))
         else:
             factor = ((self.max_lighting_distance - distance) / self.max_lighting_distance)**2
-            return np.multiply(self.color, factor)
+            return self.color * factor
 
     def get_light_vector_at(self, point):
-        light_vector = np.subtract(point, self.position)
+        light_vector = point, self.position
         light_vector /= np.linalg.norm(light_vector)
         return light_vector
 
 
 class Ambient:
     def __init__(self, color=(127, 127, 127)):
-        self.color = color
+        self.color = np.array(color)
 
     def illuminates(self, point, scene):
         return True
@@ -43,7 +43,7 @@ class Ambient:
 class Sun:
     def __init__(self, color=(127, 127, 127), direction=(0, -1, 0)):
         self.direction = direction / np.linalg.norm(direction)
-        self.color = color
+        self.color = np.array(color)
 
     def illuminates(self, point, scene):
         return not scene.check_collision(point, -self.direction)
