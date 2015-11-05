794ffb91e06c742f472663d5cbd6867d73b8cda8
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-11-05 23:01:47 +0100

    More Camera refactoring.

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import time
-from raytracer.Materials import ORANGE_GLOSSY, ORANGE_MATTE, BLUE_GLOSSY, MIRROR_GLOSSY
+import math
+import numpy
+from raytracer.Materials import ORANGE_GLOSSY, ORANGE_MATTE, BLUE_GLOSSY, MIRROR_GLOSSY, GRAY_GLOSSY, GRAY_MATTE, \
+    BLUE_MATTE
 from raytracer.Scene import Scene
 from raytracer.Camera import Camera
 from raytracer.Objects import Sphere, Plane, Circle
 from raytracer.Lights import Lamp, Ambient, Sun
 
 if __name__ == '__main__':
-    light1 = Lamp(position=(-10, 10, -10), max_lighting_distance=100)
-    light1 = Sun(direction=(0, -2, 0))
+    light1 = Sun()
     light2 = Ambient()
-
-    sphere1 = Sphere((1.5, 2.2, -4.0), 0.4, ORANGE_GLOSSY)
-    sphere2 = Sphere((-0.5, 2, -5.0), 1.2, MIRROR_GLOSSY)
-    plane1 = Plane(material=BLUE_GLOSSY)
-    circle1 = Circle(center=(0, 0.1, -5.5), radius=2, front_material=ORANGE_MATTE)
+    sphere1 = Sphere((0, 0, 0), 1, ORANGE_GLOSSY)
+    plane1 = Plane(position=(0, -2, 0), material=BLUE_GLOSSY)
 
     scene = Scene()
-
     scene.objects.append(sphere1)
-    scene.objects.append(sphere2)
     scene.objects.append(plane1)
-    scene.objects.append(circle1)
-
     scene.lights.append(light1)
     scene.lights.append(light2)
 
-    camera = Camera(eye=(0, 4, 0), center=(0, 2, -5))
-
+    camera = Camera((0, 3, 3), (0, 0, 0))
     t_start = time.time()
     print("Start")
-    camera.render_image(scene, (200, 200))
+    camera.render_image(scene, (1280, 720))
     print("Finished", time.time() - t_start)
--- raytracer/Camera.py
+++ raytracer/Camera.py
@@ -7,11 +7,11 @@ from multiprocessing import Manager
 
 
 class Camera:
-    def __init__(self, position=(0, 0, 0), look_at=(0, 0, -1), up=(0, 1, 0), horizontal_angle=45):
+    def __init__(self, position=(4, 4, 4), look_at=(0, 0, 0), up=(0, 1, 0), horizontal_angle=45):
         self.position = np.array(position)
         self.up = np.array(up)
 
-        front = np.array(look_at) - self.position
+        front = look_at - self.position
         self.front = front / np.linalg.norm(front)
         right = np.cross(self.front, self.up)
         self.right = right / np.linalg.norm(right)
@@ -19,31 +19,30 @@ class Camera:
         self.up = up / np.linalg.norm(up)
         self.image_plane_width = 2*(math.tan(math.radians(horizontal_angle/2)))
 
-    def render_image(self, scene, size=(100, 100), file_name='image.png'):
+    def render_image(self, scene, image_size=(100, 100), file_name='image.png'):
         m = Manager()
-        width, height = size
-        rendered_pixels = m.list(range(width * height))
+        rendered_pixels = m.list(range(np.prod(image_size)))
         pool = Pool()
-        calc_pix = partial(self.calculate_pixel, scene=scene, width=width, height=height, pix=rendered_pixels)
-        pool.map(calc_pix, ((x, y) for (x, y) in np.ndindex(size)))
+        pixel_color = partial(self.calculate_pixel_color, image_size=image_size, scene=scene, pixels=rendered_pixels)
+        pool.map(pixel_color, ((x, y) for (x, y) in np.ndindex(image_size)))
 
-        image = Image.new('RGB', size)
-        image.putdata(rendered_pixels)
+        rendered_image = Image.new('RGB', image_size)
+        rendered_image.putdata(rendered_pixels)
         if file_name:
-            image.save(file_name)
-        return image
+            rendered_image.save(file_name)
+        return rendered_image
 
-    def calculate_pixel(self, pixel_coordinates, scene, width, height, pix):
+    def calculate_pixel_color(self, pixel_coordinates, image_size, scene, pixels):
         x, y = pixel_coordinates
+        width, height = image_size
         pixel_vector = self.get_pixel_vector(x, y, width, height)
-        pixel_vector /= np.linalg.norm(pixel_vector)
         color = scene.trace_ray(self.position, pixel_vector)
-        pix[y * height + x] = color
+        pixels[y * width + x] = color
 
     def get_pixel_vector(self, x, y, width, height):
         image_plane_height = height * self.image_plane_width / width
-        x_pos = ((x + 0.5) / width) * self.image_plane_width - 0.5*self.image_plane_width
-        y_pos = ((y + 0.5) / height) * image_plane_height - 0.5*image_plane_height
-        pixel_vector = self.front + self.right*x_pos + self.up*-y_pos
+        pixel_pos_x = ((x + 0.5) / width) * self.image_plane_width - 0.5*self.image_plane_width
+        pixel_pos_y = ((y + 0.5) / height) * image_plane_height - 0.5*image_plane_height
+        pixel_vector = self.front + self.right*pixel_pos_x + self.up*-pixel_pos_y
         pixel_vector /= np.linalg.norm(pixel_vector)
         return pixel_vector
