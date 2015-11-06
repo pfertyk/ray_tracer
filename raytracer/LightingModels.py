6cf9865140664778cea4b8a8d2e4bffc302c4571
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-11-06 21:43:39 +0100

    Refactored LightModels.

--- raytracer/LightingModels.py
+++ raytracer/LightingModels.py
@@ -9,24 +9,28 @@ def whitted_lighting_model(scene, ray_direction, collision_result, recursion_lev
         if light.illuminates(collision_point, scene):
             light_vector = light.get_light_vector_at(collision_point)
             light_color = light.get_light_intensity_at(collision_point)
-            coeff1 = 1
             if light_vector is not None:
                 coeff1 = np.dot(-light_vector, normal)
                 coeff1 = max(coeff1, 0.0)
 
-                reflected_light_vector = _reflection(light_vector, normal)
-                coeff2 = np.dot(reflected_light_vector, -ray_direction)
-                coeff2 = max(coeff2, 0.0)
-                coeff2 = coeff2**material.phong_exponent
-                specular = [int(x * y * coeff2/255) for x, y in zip(material.specular_color, light_color)]
-                color = tuple(x + y for x, y in zip(color, specular))
-            diffColor = [int(x * y * coeff1/255) for x, y in zip(material.color, light_color)]
-            color = tuple(x + y for x, y in zip(color, diffColor))
+                specular_color = _calculate_specular_color(light_color, light_vector, material, normal, ray_direction)
+                color += specular_color
+                diffColor = [int(x * y * coeff1/255) for x, y in zip(material.color, light_color)]
+                color = tuple(x + y for x, y in zip(color, diffColor))
+            else:
+                color += material.color * light_color / 255
     if material.reflection_factor > 0:
         reflected_vector = _reflection(ray_direction, normal)
         reflected_color = scene.trace_ray(collision_point, reflected_vector, recursion_level-1)
-        color = tuple(x + y for x, y in zip(color, reflected_color))
-    return tuple(color)
+        color += np.array(reflected_color)
+    return tuple(int(c) for c in color)
+
+
+def _calculate_specular_color(light_color, light_vector, material, normal, ray_direction):
+    reflected_light_vector = _reflection(light_vector, normal)
+    specular_coefficient = max(np.dot(reflected_light_vector, -ray_direction), 0)**material.phong_exponent
+    specular = np.multiply(material.specular_color, light_color / 255) * specular_coefficient
+    return specular
 
 
 def _reflection(vector, normal):
