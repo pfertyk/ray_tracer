f71f4f66ce2b0494aba3f278885a7aead130e41c
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-11-07 16:59:59 +0100

    Added docstring to Scene.

--- raytracer/Scene.py
+++ raytracer/Scene.py
@@ -3,6 +3,13 @@ from raytracer.LightingModels import whitted_lighting_model
 
 
 class Scene:
+    """
+    Represents a scene.
+
+    Scene contains all visible objects and all light sources.
+    It also contains lighting model that represents laws of
+    physics (it is responsible for proper color calculation).
+    """
     def __init__(self):
         self.objects = []
         self.lights = []
@@ -13,6 +20,14 @@ class Scene:
         self.far = 10000
     
     def check_collision(self, eye, direction, near=None, far=None):
+        """
+        Checks if a ray collides with any object on the scene.
+        :param eye: beginning of a traced ray
+        :param direction: direction of a traced ray (must be normalized)
+        :param near: minimal distance of detecting collisions
+        :param far: maximal distance of detecting collisions
+        :return: a tuple of (collision point, normal, material) or None, if there is no collision
+        """
         if near is None:
             near = self.near
         if far is None:
@@ -29,6 +44,17 @@ class Scene:
         return collision_result
     
     def trace_ray(self, eye, direction, recursion_level=None):
+        """
+        Traces a ray of light through a scene and returns its color.
+
+        Detects first object that a ray collides with and calculated its color at given point.
+        If there is no collision, returns background color of the scene.
+        :param eye: beginning point of a traced ray
+        :param direction: direction of a traced ray (must be normalized)
+        :param recursion_level: number of times the ray can reflect from any surface to create mirror reflection
+        (if None, the scene's max_recursion_level will be used)
+        :return: color of given ray of light, as a tuple (R, G, B) (values range from 0 to 255)
+        """
         if recursion_level is None:
             recursion_level = self.max_recursion_level
         if recursion_level == 0:
